90e2bbd40f48c40270d010b6caee7c1c811d6abf
Nikita <nikitka.zhirov@gmail.com>
2020-05-04 17:50:12 +0300

    include gulp-cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function(done) {
   console.log('Привет, мир!');
@@ -14,4 +16,12 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', function (done) {
+  gulp.src('src/**/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist'));
+      done();
 });
