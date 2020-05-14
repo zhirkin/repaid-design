7b4ae18b6088bacc7018788b32e53676f555aeae
Nikita <nikitka.zhirov@gmail.com>
2020-05-15 00:37:57 +0300

    feat: add scroll up

--- js/main.js
+++ js/main.js
@@ -24,3 +24,45 @@ $(document).ready(function () {
     modal.toggleClass('modal--visible');
   });
 });
+
+(function(jq) {
+  jq.autoScroll = function(ops) {
+    ops = ops || {};
+    ops.styleClass = ops.styleClass || 'button-up';
+    var t = jq('<div class="'+ops.styleClass+'"></div>'),
+   d = jq(ops.target || document);
+   jq(ops.container || 'body').append(t);
+ 
+  t.css({
+    opacity: 0,
+    position: 'absolute',
+    top: 0,
+    right: 0
+ }).on('click', function() {
+	 jq('html,body').animate({
+	    scrollTop: 0
+   }, ops.scrollDuration || 1000);
+ });
+ 
+  d.scroll(function() {
+    var sv = d.scrollTop();
+    if (sv < 300) {
+      t.clearQueue().fadeOut(ops.hideDuration || 200);
+	 return;
+  }
+ 
+  t.css('display', '').clearQueue().animate({
+   top: sv,
+   opacity: 0.8
+   }, ops.showDuration || 500);
+  });
+  };
+})(jQuery);
+ 
+$(document).ready(function(){
+ $.autoScroll({
+ scrollDuration: 600, 
+ showDuration: 600, 
+ hideDuration: 300
+ });
+});
