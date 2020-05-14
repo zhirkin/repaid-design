69a6da3d17560e514461ca2324b68b9b45e88bec
Nikita <nikitka.zhirov@gmail.com>
2020-05-14 22:41:07 +0300

    feat: add jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -10,3 +11,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
   closeBtn.addEventListener('click', switchModal);
   });
+*/
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+});
